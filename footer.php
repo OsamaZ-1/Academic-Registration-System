a13f07d688207f7422e5423a561b1098c72b9f2d
Obaida Ammar <96881150+ObaidaAmmar@users.noreply.github.com>
2023-05-19 01:38:32 +0300

    navbar and footer

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php 
+      echo '<footer class="footer">
+              <div class="lu-contact">
+                <h1>Contact Us</h1>
+                <p>Address: RGHC+4PR, Beirut</p>
+                <p>Phone: 01 612 831</p>
+                <p><i class="fa-brands fa-facebook" style="color: #0056d6;"></i><a href="https://www.facebook.com/ul.edu.lb" target="_blank">Facebook</a></p>
+                <p><i class="fa-brands fa-instagram" style="color: #a40df8;"></i><a href="" target="_blank">Instagram</a></p>
+                <p><i class="fa-brands fa-twitter" style="color: #00d5f3;"></i><a href="https://twitter.com/LebaneseUni" target="_blank">Twitter</a></p>
+                <p><i class="fa-brands fa-youtube" style="color: #ff2600;"></i><a href="https://www.youtube.com/c/LebaneseUniversityOfficialChannel" target="_blank">Youtube</a></p>
+              </div>
+              <div class="lu-campuses">
+                <h1>Faculty of Science Campuses</h1>
+                <p><h2>Branch 1 : </h2>Mount Lebanon, Hadath</p>
+                <p><h2>Branch 2 : </h2>Mount Lebanon, Fanar</p>
+                <p><h2>Branch 3 : </h2>North Governorate, Tripoli</p>
+                <p><h2>Branch 4 : </h2>Beqaa Governorate, Zahle</p>
+                <p><h2>Branch 5 : </h2>Nabatieh Governorate</p>
+              </div>
+              <div class="developers">
+                <h1>Developed By</h1>
+                <p><i class="fa-brands fa-linkedin" style="color: #0056d6;"></i><a href="https://www.linkedin.com/in/obaida-ammar" target="_blank">Obaida Ammar</a></p>
+                <p><i class="fa-brands fa-linkedin" style="color: #0056d6;"></i><a href="" target="_blank">Osama Zammar</a></p>
+                <p><i class="fa-brands fa-linkedin" style="color: #0056d6;"></i><a href="" target="_blank">Mohammad Abo Alfoul</a></p>
+              </div>
+            </footer>';
+?>
---
+++ header.php
@@ -0,0 +1,19 @@
+<?php 
+      echo '<header>
+              <nav class="navbar">
+                <div class="logo-title">
+                  <figure class="logo">
+                    <img src="images/LU-logo.png" width="500" height="500" alt="LU-logo">
+                  </figure>
+                  <h1><a href="index.php">LU-STDRG<a></h1>
+                </div>
+                <div class="links">
+                  <ul>
+                    <li><a href="index.php">Home</a></li>
+                  </ul>
+                  <button id="login"><a href="login.php">Login</a></button>
+                  <button id="signup"><a href="signup.php">SignUp</a></button>
+                </div>
+              </nav>
+            </header>';
+?>
