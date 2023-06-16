1ca83642c074de2020a5f19aeb0236363fd81a9a
Obaida Ammar <96881150+ObaidaAmmar@users.noreply.github.com>
2023-06-16 15:23:06 +0300

    some modifications to user-page

--- user-page.php
+++ user-page.php
@@ -47,110 +47,119 @@
  <main>
   <h1 class="page-title">Courses Registration</h1>
   <div class="container">
-  <div>
-    <input type='text' id='credit-counter' value='0' disabled/>
-  </div>
-  <div>
-    <div class="year-title"><h3>First Year Courses</h2><button class="arrow-buttons" onclick="toggleDiv(1)"><i class="fa-solid fa-angle-up fa-xl"></i></button></div>
-    <div id="table-div-1" class="table-toggle">
+    <form action="" method="POST">
       <div>
-        <p>1st Semester: </p>
-        <table id="firstYear1" class="courses-table">
-          <?php
-            $temp_course_year = 1;
-            $temp_course_sem = 1;
-            require("templates/fill_course_tables.php");
-          ?>
-        </table>
+        <input type='text' id='credit-counter' value='0' disabled/>
       </div>
+      <!--First Year Section -->
       <div>
-        <p>2nd Semester: </p>
-        <table id="firstYear2" class="courses-table">
-          <?php
-            $temp_course_year = 1;
-            $temp_course_sem = 2;
-            require("templates/fill_course_tables.php");
-          ?>
-        </table>
+        <div class="year-title"><h3>First Year Courses</h2><button type="button" class="arrow-buttons" onclick="toggleDiv(1)"><i class="fa-solid fa-angle-up fa-xl"></i></button></div>
+        <div id="table-div-1" class="table-toggle">
+          <div>
+            <p>1st Semester: </p>
+            <table id="firstYear1" class="courses-table">
+              <?php
+                $temp_course_year = 1;
+                $temp_course_sem = 1;
+                require("templates/fill_course_tables.php");
+              ?>
+            </table>
+          </div>
+          <div>
+            <p>2nd Semester: </p>
+            <table id="firstYear2" class="courses-table">
+              <?php
+                $temp_course_year = 1;
+                $temp_course_sem = 2;
+                require("templates/fill_course_tables.php");
+              ?>
+            </table>
+          </div>
+        </div>
+      </div>
+      <!--Second Year Section -->
+      <div>
+        <div class="year-title"><h3>Second Year Courses</h2> <button type="button" class="arrow-buttons" onclick="toggleDiv(2)"><i class="fa-solid fa-angle-up fa-xl"></i></button></div>
+        <div id="table-div-2" class="table-toggle">
+          <div>
+            <p>1st Semester: </p>
+            <table id="secondYear1" class="courses-table">
+              <?php
+                $temp_course_year = 2;
+                $temp_course_sem = 1;
+                require("templates/fill_course_tables.php");
+              ?>
+            </table>
+          </div>
+          <div>
+            <p>2nd Semester:</p>
+            <table id="secondYear2" class="courses-table">
+              <?php
+                $temp_course_year = 2;
+                $temp_course_sem = 2;
+                require("templates/fill_course_tables.php");
+              ?>
+            </table>
+          </div>
+        </div>
+      </div>
+      <!--Third Year Section -->
+      <div>
+        <div class="year-title"><h3>Third Year Courses</h2> <button type="button" class="arrow-buttons" onclick="toggleDiv(3)"><i class="fa-solid fa-angle-up fa-xl"></i></button></div>
+        <div id="table-div-3" class="table-toggle">
+          <div>
+            <p>1st Semester: </p>
+            <table id="thirdYear1" class="courses-table">
+              <?php
+                $temp_course_year = 3;
+                $temp_course_sem = 1;
+                require("templates/fill_course_tables.php");
+              ?>
+            </table>
+          </div>
+          <div>
+            <p>2nd Semester:</p>
+            <table id="thirdYear2" class="courses-table">
+              <?php
+                $temp_course_year = 3;
+                $temp_course_sem = 2;
+                require("templates/fill_course_tables.php");
+              ?>
+            </table>
+         </div>
+       </div>
+     </div>
+     <!--Fourth Year Section -->
+     <div>
+      <div class="year-title"><h3>Fourth Year Courses</h2> <button type="button" class="arrow-buttons" onclick="toggleDiv(4)"><i class="fa-solid fa-angle-up fa-xl"></i></button></div>
+      <div id="table-div-4" class="table-toggle">
+        <div>
+          <p>1st Semester: </p>
+          <table id="fourthYear1" class="courses-table">
+            <?php
+              $temp_course_year = 4;
+              $temp_course_sem = 1;
+              require("templates/fill_course_tables.php");
+            ?>
+          </table>
+        </div>
+        <div>
+          <p>2nd Semester:</p>
+          <table id="fourthYear2" class="courses-table">
+            <?php
+              $temp_course_year = 4;
+              $temp_course_sem = 2;
+              require("templates/fill_course_tables.php");
+            ?>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
-  
-  <div>
-    <div class="year-title"><h3>Second Year Courses</h2> <button class="arrow-buttons" onclick="toggleDiv(2)"><i class="fa-solid fa-angle-up fa-xl"></i></button></div>
-    <div id="table-div-2" class="table-toggle">
-      <div>
-        <p>1st Semester: </p>
-        <table id="secondYear1" class="courses-table">
-          <?php
-            $temp_course_year = 2;
-            $temp_course_sem = 1;
-            require("templates/fill_course_tables.php");
-          ?>
-        </table>
-      </div>
-      <div>
-        <p>2nd Semester:</p>
-        <table id="secondYear2" class="courses-table">
-          <?php
-            $temp_course_year = 2;
-            $temp_course_sem = 2;
-            require("templates/fill_course_tables.php");
-          ?>
-        </table>
-      </div>
+    <div class="register-courses">
+      <textarea name="msg" class="message" cols="30" rows="10" placeholder="Enter Message Here!"></textarea>
+      <input type="submit" name="submit" value="Register Courses" />
     </div>
-  </div>
-  <div>
-  <div class="year-title"><h3>Third Year Courses</h2> <button class="arrow-buttons" onclick="toggleDiv(3)"><i class="fa-solid fa-angle-up fa-xl"></i></button></div>
-    <div id="table-div-3" class="table-toggle">
-      <div>
-        <p>1st Semester: </p>
-        <table id="thirdYear1" class="courses-table">
-          <?php
-            $temp_course_year = 3;
-            $temp_course_sem = 1;
-            require("templates/fill_course_tables.php");
-          ?>
-        </table>
-      </div>
-      <div>
-        <p>2nd Semester:</p>
-        <table id="thirdYear2" class="courses-table">
-          <?php
-            $temp_course_year = 3;
-            $temp_course_sem = 2;
-            require("templates/fill_course_tables.php");
-          ?>
-        </table>
-      </div>
-    </div>
-  </div>
-  <div>
-  <div class="year-title"><h3>Fourth Year Courses</h2> <button class="arrow-buttons" onclick="toggleDiv(4)"><i class="fa-solid fa-angle-up fa-xl"></i></button></div>
-    <div id="table-div-4" class="table-toggle">
-      <div>
-        <p>1st Semester: </p>
-        <table id="fourthYear1" class="courses-table">
-          <?php
-            $temp_course_year = 4;
-            $temp_course_sem = 1;
-            require("templates/fill_course_tables.php");
-          ?>
-        </table>
-      </div>
-      <div>
-        <p>2nd Semester:</p>
-        <table id="fourthYear2" class="courses-table">
-          <?php
-            $temp_course_year = 4;
-            $temp_course_sem = 2;
-            require("templates/fill_course_tables.php");
-          ?>
-        </table>
-      </div>
-    </div>
-  </div>
+   </form> 
   </div>
   <input type="hidden" id="max-allowed-credits" value="<?php if ($student_year <= 2) echo 60; else echo 72;?>" />
  </main>
