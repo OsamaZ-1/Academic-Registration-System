f780bf0c4801f10d3a1f0f38cc1960eea310e6c5
Obaida Ammar <96881150+ObaidaAmmar@users.noreply.github.com>
2023-07-21 20:18:43 +0300

    fixed import accounts sweet alert

--- Actions/import_students_accounts.php
+++ Actions/import_students_accounts.php
@@ -4,22 +4,22 @@
   require_once '../vendor/autoload.php'; 
   use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
-        
-    if(isset($_POST['importSubmit']))
-    { 
-        
+    
+    if(isset($_FILES['file']))
+    {       
+            require("../classes/dal.php");
             require("../classes/student_dal.php");
             $student_dal=new Student_DAL();
+
             // Allowed mime types 
             $excelMimes = array('text/xls', 'text/xlsx', 'application/excel', 'application/vnd.msexcel', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'); 
    
             // Validate whether selected file is a Excel file 
             if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $excelMimes))
             { 
-       
              // If the file is uploaded 
              if(is_uploaded_file($_FILES['file']['tmp_name']))
-             { 
+             {  
                 $reader = new Xlsx(); 
                 $spreadsheet = $reader->load($_FILES['file']['tmp_name']); 
                 $worksheet = $spreadsheet->getActiveSheet();  
@@ -27,9 +27,9 @@
 
                 // Remove header row 
                 unset($worksheet_arr[0]); 
-
+                $finalResult = 0;
                 foreach($worksheet_arr as $row)
-                { 
+                {  
                     $student_id = $row[0];
                     $first_name = $row[1]; 
                     $last_name = $row[2]; 
@@ -44,36 +44,39 @@
                     // Check whether member already exists in the database with the same email 
                     $email_exist = $student_dal -> isEmailExist($student_id,$student_email);
 
-                    $result = null;
+                    
                     if($id_exist && !$email_exist)
                     { 
                         // Update member data in the database 
                         $result = $student_dal -> updateStudentAccount($student_id, $first_name, $last_name, $student_email, $student_password, $student_major, $student_year, $student_enrollment_date);
-                 
+                        $finalResult++;
                     }else if(!$id_exist && !$email_exist)
                     { 
                         // Insert member data in the database 
                         $result = $student_dal -> addStudent($student_id, $first_name, $last_name, $student_email, $student_password, $student_major, $student_year, $student_enrollment_date);
-                  
-                    } 
-
-                    if($result==1)
+                        $finalResult++;
+                    }
+                    
+            
+                    
+                } 
+                if($finalResult>0)
                     {
                         $v=array(
                         'result'=>true,
                         'error'=>'no error'
                         );
                     }
-                    else 
+                else 
                     {
                         $v=array(
                         'result'=>false,
-                        'error'=>$result
+                        'error'=>'exist in database'
                         );
                     }
                     $dataResult=$v;
                     echo json_encode($dataResult);
-                } 
+                
             }
            }
         }
